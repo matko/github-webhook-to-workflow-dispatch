ca9fd13f5c70395d5c9a9ff231c6bc7e060ffeff
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-07-28 20:53:23 +0200

    move prolog code to subdir

--- package.nix
+++ package.nix
@@ -9,7 +9,7 @@ stdenvNoCC.mkDerivation (attrs: {
   buildInputs = [
     swiProlog
   ];
-  src = ./.;
+  src = ./prolog;
 
   buildPhase = ''
 swipl -o ${attrs.name} -c github-webhook-to-workflow-dispatch.pl
